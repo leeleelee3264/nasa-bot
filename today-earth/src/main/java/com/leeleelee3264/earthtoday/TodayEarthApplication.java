5b9486a5a6d3a46c1d8d880e9f00b9dc22e52094
leeleelee3264 <absinthe4902@naver.com>
2022-12-29 14:38:57 +0900

    In the middle of download earth image

--- today-earth/src/main/java/com/leeleelee3264/earthtoday/TodayEarthApplication.java
+++ today-earth/src/main/java/com/leeleelee3264/earthtoday/TodayEarthApplication.java
@@ -1,30 +1,59 @@
 package com.leeleelee3264.earthtoday;
 
 import com.leeleelee3264.earthtoday.hello.GreetingClient;
+import com.leeleelee3264.earthtoday.nasa.archive.ArchiveClient;
 import com.leeleelee3264.earthtoday.nasa.meta.Meta;
 import com.leeleelee3264.earthtoday.nasa.meta.MetaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
 public class TodayEarthApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(TodayEarthApplication.class);
+
+    @Value("${earth.image.directory}")
+    private static String earthImageDirectory;
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(TodayEarthApplication.class, args);
 
+        LocalDate date = LocalDate.now().minusDays(3);
+        String dirName = earthImageDirectory + "/" + date.toString();
 
-        MetaClient metaClient = context.getBean(MetaClient.class);
-        List<Meta> m = metaClient.get(LocalDate.now().minusDays(3));
-
-        for(Meta mm: m) {
-            System.out.println(mm.getImagePath());
+        try {
+            Files.createDirectories(Paths.get(dirName));
+        } catch (IOException e) {
+            // TODO: log를 {} 써서 포메터로 예쁘게 뽑는 법이 있었다.
+            log.error(e.getMessage());
         }
 
-        System.out.println(m.size());
+//        MetaClient metaClient = context.getBean(MetaClient.class);
+//
+//        List<Meta> m = metaClient.get(date);
+//
+//        for(Meta mm: m) {
+//            System.out.println(mm.getImagePath());
+//        }
+//
+//        System.out.println(m.size());
+
+        // TODO 디렉터리 만드는 곳, 이 meta, archive를 부르는 api가 있어야 한다.
+        String t = "https://api.nasa.gov/EPIC/archive/natural/2022/10/22/png/epic_1b_20221022004554.png?api_key=aumlN0C5xNVPnqlSHE3PVhHxEzrfm53Entwl6yhi";
+
+        ArchiveClient archiveClient = context.getBean(ArchiveClient.class);
+        archiveClient.get(date, t);
 
 
     }
--- today-earth/src/main/java/com/leeleelee3264/earthtoday/nasa/archive/ArchiveClient.java
+++ today-earth/src/main/java/com/leeleelee3264/earthtoday/nasa/archive/ArchiveClient.java
@@ -3,9 +3,20 @@ package com.leeleelee3264.earthtoday.nasa.archive;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.datetime.joda.DateTimeFormatterFactoryBean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Component
 public class ArchiveClient {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -18,12 +29,20 @@ public class ArchiveClient {
     private final String subUrl = "/EPIC/archive/natural/";
 
     private final RestTemplate restTemplate;
-    private final HttpHeaders headers;
 
     public ArchiveClient() {
-        restTemplate = new RestTemplate();
+        this.restTemplate = new RestTemplate();
+    }
 
-        headers = new HttpHeaders();
+    public void get(LocalDate date, String url) {
+//        String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+//        String url = apiUrl + subUrl + formattedDate + "/png/" + imagePath +  "?api_key=" + apiKey;
+
+        File file = restTemplate.execute(url, HttpMethod.GET, null, clientHttpResponse -> {
+            File ret = File.createTempFile("download", "tmp");
+            StreamUtils.copy(clientHttpResponse.getBody(), Files.newOutputStream(ret.toPath()));
+            return ret;
+        });
 
     }
 }
