4379160078816ed85510cdc56b0fd79f017930b8
leeleelee3264 <absinthe4902@naver.com>
2023-01-11 18:45:25 +0900

    In the middle of Docker

--- today-earth/src/main/java/com/leeleelee3264/earthtoday/nasa/task/EarthTask.java
+++ today-earth/src/main/java/com/leeleelee3264/earthtoday/nasa/task/EarthTask.java
@@ -17,7 +17,8 @@ public class EarthTask {
         this.earthService = earthService;
     }
 
-    @Scheduled(cron = "0 59 23 * * *")
+//    @Scheduled(cron = "0 59 23 * * *")
+    @Scheduled(cron = "0 35 18 * * *")
     public void triggerEarthBot() {
         // 당일이나 전날 데이터를 조회하면 NASA에서 자료를 올려주지 않은 경우가 많아서 2일 정도 전의 데이터를 조회한다.
         LocalDate today = LocalDate.now();
